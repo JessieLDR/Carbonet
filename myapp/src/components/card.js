cea1d1da3d5ed368b56d5204a3ce6d893b4be0e6
JessieLDR <jessicaliuderong@gmail.com>
2022-03-27 12:01:07 -0700

    card, fitler

--- myapp/src/components/card.js
+++ myapp/src/components/card.js
@@ -1,34 +1,43 @@
-import blog from "../images/blog.png";
+{/* <div className="card">
+<div className="blog-post-hero blog-post-hero--short">
+<img className="blogPhoto" src={blog}></img>
+</div>
+<div className="blog__title blog__title--small">
+<h2>Event Partner Finder</h2>
+</div>
+<div className="blog__author">
+<p className="authorName">
+@Golanginya
+</p>
+<p className="blog__author-title">12 November 2020 19:35</p>
+</div>
 
-function Card(props) {
-    return (
-    <div className="card">
-      {/* blog card */}
-      <div className="blog-post-hero blog-post-hero--short">
-        <img className="blogPhoto" src={blog}></img>
-      </div>
-      <div className="blog__title blog__title--small">
-        <h2>Event Partner Finder</h2>
-      </div>
-      <div className="blog__author">
-        <p className="authorName">
-        @Golanginya
-        </p>
-        <p className="blog__author-title">12 November 2020 19:35</p>
-      </div>
+<div className="blog-content"> 
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Ornare rutrum amet, a nunc mi lacinia in iaculis. 
+Pharetra ut integer nibh urna. Placerat ut adipiscing nulla lectus vulputate massa, 
+fscelerisque. Netus nisl nulla placerat dignissim ipsum arcu.
+</p>
+</div>
+<div className="blog__read-more">
+<a>Read More</a>
+</div>
 
-      <div className="blog-content"> 
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Ornare rutrum amet, a nunc mi lacinia in iaculis. 
-        Pharetra ut integer nibh urna. Placerat ut adipiscing nulla lectus vulputate massa, 
-        fscelerisque. Netus nisl nulla placerat dignissim ipsum arcu.
-        </p>
-      </div>
-      <div className="blog__read-more">
-        <a>Read More</a>
-      </div>
-     </div>
-    )
-}
-export default Card;
+</div>
+<div className="card">
+<div className="blog__author">
+  <p className="authorName">
+  @unkind
+  </p>
+  <p className="blog__author-title">12 November 2020 19:35</p>
+</div>
+<div className="blog-content"> 
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Ornare rutrum amet, a nunc mi lacinia in iaculis. 
+  Pharetra ut integer nibh urna. Placerat ut adipiscing nulla lectus vulputate massa, 
+  fscelerisque. Netus nisl nulla placerat dignissim ipsum arcu.
+  </p>
+</div>
+</div> */}
