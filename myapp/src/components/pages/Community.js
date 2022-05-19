3ed512cf61c4938776affcb1af314a2afb3754c7
JessieLDR <jessicaliuderong@gmail.com>
2022-05-19 16:56:00 -0700

    community look fixed

--- myapp/src/components/pages/Community.js
+++ myapp/src/components/pages/Community.js
@@ -1,29 +1,15 @@
 import * as React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 
 import Card from "../general/card";
 import { Pagination } from '@mui/material';
 import { Container } from '@mui/material';
 
-import { useNavigate } from 'react-router-dom';
-import PostHeading from "../images/PostHeading.png";
-
 export default function ResponsiveGrid() {
-  let navigate = useNavigate();
   return (
     <div>
-    <img src='PostHeading'></img>
     <Container maxWidth="md">
-    <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={{ xs: 4, md: 6 }} columns={{ xs: 3, sm: 4, md: 4 }}>
-            <Grid item xs={5} sm={10} md={10}>
-                <Card></Card>
-            </Grid>
-        </Grid>
-    </Box>
+      {/* <a href="/somewhere"><AddIcon /></a> */}
+      <Card></Card>
     </Container>
     <Pagination sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}
     count={3} size="large" />
